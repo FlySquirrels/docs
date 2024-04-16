afb30afcd2f49a6334f2cc71073ce2692df2bc14
YunXin <YunXin@cc.com>
2024-04-16 13:46:44 +0800

    init

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  title: 'yunsheng:云生',
+  title: 'yunsheng',
   description: '月下飞天镜，云生结海楼。',
   head:[
     [
@@ -9,5 +9,20 @@ module.exports = {
         href:'/favicon.ico'
       }
     ]
-  ]
+  ],
+  nav: [
+    {text:'主页', link:'/'},
+    {text:'实用工具', link:'/tools'} 
+  ],
+  sidebar: {
+    '/tools/':[
+      {
+        title: '实用工具',
+        collapsable: false,
+        children:[
+          {title: '自动构建项目',path:'/tools/autobuild01'}
+        ]
+      }
+    ]
+  }
 }
--- docs/README.md
+++ docs/README.md
@@ -4,7 +4,7 @@ heroImage: /yunsheng.jpg
 heroText: YunSheng
 tagline: 归纳、管理项目文档
 actionText: 最新项目 →
-actionLink: /zh/guide/
+actionLink: /
 features:
 - title: 云生
   details: 出自“月下飞天镜，云生结海楼。”
@@ -12,5 +12,11 @@ features:
   details: 中文翻译:"发出嗡嗡声",原来云朵里面还盛产小蜜蜂。
 - title: 文档
   details: 管理项目中的功能介绍等，多个项目组合而成的文档。
+features:
+- title: 简单实用工具
+  details: 服务器自动拉取github代码编译，减少不必要的部署流程。
+  actionText: 直达
+  actionText: 最新项目 →
+  actionLink: /tools
 footer: Copyright © 2024-present Yun Sheng
 ---
